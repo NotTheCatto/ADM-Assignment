71c3c8d81aaac64629ca0eabe8444794de731e96
NotTheCatto <ykwho098@gmail.com>
2025-03-04 11:25:37 +0800

    feat(tables): Add create tables sql scripts

---
+++ sqlscripts/tables/createTables.sql
@@ -0,0 +1,49 @@
+DROP TABLE Address
+DROP TABLE Contact
+DROP TABLE Publisher
+DROP TABLE StaffRole
+DROP TABLE Staff
+DROP TABLE StaffPerformanceReview
+DROP TABLE LibraryBranch
+DROP TABLE ShiftType
+DROP TABLE StaffShiftSchedule
+DROP TABLE MembershipType
+DROP TABLE StatusCode
+DROP TABLE Membership
+DROP TABLE Borrower
+DROP TABLE Author
+DROP TABLE BookAuthor
+DROP TABLE Category
+DROP TABLE BookCategory
+DROP TABLE LanguageCode
+DROP TABLE Book
+DROP TABLE BookCopy
+DROP TABLE Loan
+DROP TABLE Fine
+DROP TABLE Invoice
+DROP TABLE Payment
+
+source tableAddress.sql;
+source tableContact.sql;
+source tablePublisher.sql;
+source tableStaffRole.sql;
+source tableStaff.sql;
+source tableStaffPerformanceReview.sql;
+source tableLibraryBranch.sql;
+source tableShiftType.sql;
+source tableStaffShiftSchedule.sql;
+source tableMembershipType.sql;
+source tableStatusCode.sql;
+source tableMembership.sql;
+source tableBorrower.sql;
+source tableAuthor.sql;
+source tableBookAuthor.sql;
+source tableCategory.sql;
+source tableBookCategory.sql;
+source tableLanguageCode.sql;
+source tableBook.sql;
+source tableBookCopy.sql;
+source tableLoan.sql;
+source tableFine.sql;
+source tableInvoice.sql;
+source tablePayment.sql;
