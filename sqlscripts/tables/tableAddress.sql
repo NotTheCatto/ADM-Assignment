689e2cd35256f5590215361451ce0301038a3b4c
NotTheCatto <ykwho098@gmail.com>
2025-03-10 00:55:54 +0800

    feat: Add create Address table statements

--- sqlscripts/tables/tableAddress.sql
+++ sqlscripts/tables/tableAddress.sql
@@ -1,1 +1,11 @@
 PROMPT Creating table Address
+
+CREATE TABLE Address(
+  AddressID	NUMBER(4)     NOT NULL,
+  Street	VARCHAR2(20)  NOT NULL,
+  City		VARCHAR2(20)  NOT NULL,
+  PostalCode 	NUMBER(5)     NOT NULL,
+  State 	VARCHAR2(14)  NOT NULL,
+  Country 	VARCHAR(9,2)  NOT NULL,
+  PRIMARY KEY (AddressID)
+);
