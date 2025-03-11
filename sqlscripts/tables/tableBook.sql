6d284fcf04bed0e78a441defaa18e71b9e705504
NotTheCatto <ykwho098@gmail.com>
2025-03-11 11:11:03 +0800

    feat: Add Book table DDL

--- sqlscripts/tables/tableBook.sql
+++ sqlscripts/tables/tableBook.sql
@@ -1,1 +1,15 @@
 PROMPT Creating table Book
+
+CREATE TABLE Book(
+  BookID	  NUMBER(4)     NOT NULL,
+  PublisherID	  NUMBER(4)     NOT NULL,
+  LanguageCode	  NUMBER(4)     NOT NULL,
+  Title 	  VARCHAR2(20)  NOT NULL,
+  ISBN  	  VARCHAR2(20)  NOT NULL,
+  PublicationYear NUMBER(4)     NOT NULL,
+  BookDescription VARCHAR2(700) NOT NULL,
+
+  PRIMARY KEY (BookID),
+  FOREIGN KEY (PublisherID) REFERENCES Publisher (PublisherID),
+  FOREIGN KEY (LanguageCode) REFERENCES LanguageCode (LanguageCode)
+);
