efadd74cd2cfa88192485697124ff061aa22e1e5
NotTheCatto <ykwho098@gmail.com>
2025-04-22 13:09:12 +0800

    feat: Add todo comments

--- sqlscripts/procedures/bei_prodecure1.sql
+++ sqlscripts/procedures/bei_prodecure1.sql
@@ -2,3 +2,37 @@
 Boon Earn Iie - Procedure 1: Loan books to borrowers
 */
 
+CREATE OR REPLACE PROCEDURE staff_performance_report IS
+    -- (Set parameters: StaffID)
+
+    -- Declare variables
+
+BEGIN
+    -- Set variables
+
+    -- Loop
+
+        -- Prompt borrower's BorrowerID
+
+        -- Loop
+
+            -- Prompt books to be loaned (Prompt BookCopyId)
+
+            -- Start transaction
+
+            -- Set status code for BookCopyID record
+
+            -- Get current date
+
+            -- Calculate due date
+
+            -- Create Loan record
+
+            -- Commit transaction
+
+        -- End loop
+
+    -- End loop
+
+END;
+/
--- sqlscripts/procedures/bei_prodecure2.sql
+++ sqlscripts/procedures/bei_prodecure2.sql
@@ -2,3 +2,15 @@
 Boon Earn Iie - Procedure 2: Assign/reassign existing staff to a shift schedule
 */
 
+CREATE OR REPLACE PROCEDURE staff_performance_report IS
+    -- (Set parameters: StaffID)
+
+    -- Declare variables
+
+BEGIN
+    -- Prompt StaffID to assign schedule
+
+    -- ...
+
+END;
+/
