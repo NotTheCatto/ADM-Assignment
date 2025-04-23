ef2fe99a4cac248b6ea436eeae9ee2433eba06a8
Cat on WSL <ykwho098@gmail.com>
2025-04-23 22:40:48 +0800

    fix: Fix procedure 1

--- sqlscripts/procedures/bei_prodecure1.sql
+++ sqlscripts/procedures/bei_prodecure1.sql
@@ -1,3 +1,7 @@
+/*
+Boon Earn Iie - Procedure 1: Loan books to borrowers
+*/
+
 SET SERVEROUTPUT ON;
 CREATE OR REPLACE PROCEDURE borrower_loan_books(
 	p_StaffID Staff.StaffID%TYPE,
@@ -5,46 +9,41 @@ CREATE OR REPLACE PROCEDURE borrower_loan_books(
 	p_BookCopyID BookCopy.BookCopyID%TYPE
 )IS
 	-- Declare cursor variables
-	TYPE bookcopy_cursor_type IS REF_CURSOR;
+	TYPE bookcopy_cursor_type IS REF CURSOR;
 	bookcopy_cursor bookcopy_cursor_type;
 
 	-- Declare variables
-	v_LOANDATE Loan.LoanDate%TYPE;
-	v_DUEDATE Loan.DueDate%TYPE;
+	v_LoanDate Loan.LoanDate%TYPE;
+	v_DueDate Loan.DueDate%TYPE;
 	v_StatusCode BookCopy.StatusCode%TYPE;
+	v_code NUMBER;
+	v_errMsg VARCHAR2(64);
 
 	-- Declare exceptions
 	copy_not_available EXCEPTION;
 BEGIN
-	-- Print param variables
-	DBMS_OUTPUT.PUT_LINE('StaffID: ' || p_StaffID);
-	DBMS_OUTPUT.PUT_LINE('BorrowerID: ' || p_BorrowerID);
-	DBMS_OUTPUT.PUT_LINE('BookCopyID: ' || p_BookCopyID);
-
 	-- Set variables
-	OPEN bookcopy_cursor FOR 'SELECT StatusCode FROM BookCopy WHERE BookCopyID=p_BookCopyID';
+	OPEN bookcopy_cursor FOR 'SELECT StatusCode FROM BookCopy WHERE BookCopyID=' || p_BookCopyID;
 
 	-- Start transaction
 	-- Check if status code is 1
 	FETCH bookcopy_cursor INTO v_StatusCode;
 	CLOSE bookcopy_cursor;
-	IF v_StatusCode = 1 THEN
-		-- Set status code for BookCopyID record
-		UPDATE BookCopy
-		SET StatusCode=2 -- 2 for 'Reserved' bazed on sample records
-		WHERE BookCopyID=p=BookCopyID;
-	
-		-- Get current date
-		v_LOANDATE := SYSDATE;
-	
-		-- Calculate due date
-		v_DUEDATE := ADDDAYS(SYSDATE, 14);
+	IF ( v_StatusCode = 1 ) THEN
+		-- Status is set to 2 as unavailable
+		UPDATE BookCopy SET StatusCode = 2 WHERE BookCopyID=p_BookCopyID;
 	
 		-- Create Loan record
-		INSERT INTO Loan (LoanID, BorrowerID, BookCopyID, LoanProcessedByStaffID, ReturnProcessedByStaffID, LoanDate, DueDate, ReturnDate) VALUES (SEQ_LOAN, p_BorrowerID, p_BookCopyID, p_StaffID, NULL, v_LOANDATE, v_DUEDATE, NULL);
+		INSERT INTO Loan (LoanID, BorrowerID, BookCopyID, LoanProcessedByStaffID, ReturnProcessedByStaffID, LoanDate, DueDate, ReturnDate) VALUES (SEQ_LOAN.nextval, p_BorrowerID, p_BookCopyID, p_StaffID, NULL, SYSDATE, ( SYSDATE + 14 ), NULL);
 	
 		-- Commit transaction
 		Commit;
+
+		-- Print success message
+		DBMS_OUTPUT.PUT_LINE('--- SUCCESS ---');
+		DBMS_OUTPUT.PUT_LINE('Borrower ID: ' || p_BorrowerID);
+		DBMS_OUTPUT.PUT_LINE('Book borrowed (ID): ' || p_BookCopyID);
+		DBMS_OUTPUT.PUT_LINE('Proccessed by Staff (ID): ' || p_StaffID);
 	ELSE
 		RAISE copy_not_available;
 	END IF;
@@ -53,15 +52,14 @@ BEGIN
 EXCEPTION
 	WHEN copy_not_available THEN
 		DBMS_OUTPUT.PUT_LINE('--- ERROR ---');
-		DBMS_OUTPUT.PUT_LINE('BookCopy ''' || p_BookCopyID || ''' is not available for loan.');
+		DBMS_OUTPUT.PUT_LINE('BookCopy with the ID ''' || p_BookCopyID || ''' is not available for loan.');
 	WHEN others THEN
+		v_code := SQLCODE;
+		v_errMsg := SUBSTR(SQLERRM, 1, 64);
 		DBMS_OUTPUT.PUT_LINE('--- ERROR ---');
 		DBMS_OUTPUT.PUT_LINE('An exception has occured.');
+		DBMS_OUTPUT.PUT_LINE('Error code: ' || v_code);
+		DBMS_OUTPUT.PUT_LINE('Error message: ' || v_errmsg);
 		ROLLBACK;
-
 END;
 /
-/*
-Boon Earn Iie - Procedure 1: Loan books to borrowers
-*/
-
