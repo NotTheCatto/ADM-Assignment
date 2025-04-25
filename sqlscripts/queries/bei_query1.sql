a50c7fe8f3cbe0630260170920c515b40a7bd1ba
Cat on WSL <ykwho098@gmail.com>
2025-04-25 16:09:53 +0800

    feat: Complete sql for query 1

--- sqlscripts/queries/bei_query1.sql
+++ sqlscripts/queries/bei_query1.sql
@@ -2,14 +2,24 @@
 Boon Earn Iie - Query 1: Summary of staff shift schedule and details of assigned staff
 */
 CLEAR COLUMNS
+CLEAR TTITLE
+TTITLE ON
+COLUMN ShiftTypeID FORMAT 999 JUSTIFY LEFT
+COLUMN ShiftName FORMAT A9 JUSTIFY LEFT
+COLUMN StartTime FORMAT A9 JUSTIFY LEFT
+COLUMN EndTime FORMAT A9 JUSTIFY LEFT
+COLUMN NumOfStaff FORMAT 999999999 JUSTIFY LEFT
+COLUMN StaffAvgScore FORMAT 999.99 JUSTIFY LEFT
+
 -- Set page size and line size
 SET PAGESIZE 1000
-SET LINESIZE 150
-COLUMN "Summary of staff shift schedule and assigned staff" FORMAT A20 JUSTIFY CENTER
+SET LINESIZE 500
+TTITLE CENTER ' --- Summary of staff shift schedule and assigned staff --- ' FORMAT A20 SKIP 2 LEFT 'Page: ' SQL.PNO SKIP 1;
 
-SELECT type.ShiftName, library.BranchName, schedule.ShiftStartTime, schedule.ShiftEndTime, COUNT(),
-FROM StaffShiftSchedule schedule
-INNER JOIN ShiftType type ON schedule.ShiftTypeID=type.ShiftTypeID
-INNER JOIN LibraryBranch library ON schedule.LibraryBranchID=library.LibraryBranchID
-GROUP BY ShiftStartTime, ShiftEndTime, ShiftDay
-ORDER BY ShiftDay;
+SELECT t.ShiftTypeID, t.ShiftName, TO_CHAR(t.ShiftStartTime,'hh24:mi:ss') AS StartTime, TO_CHAR(t.ShiftEndTime,'hh24:mi:ss') AS EndTime, COUNT(sc.StaffID) AS NumOfStaff, ROUND(AVG(r.ReviewScore), 2) AS StaffAvgScore
+FROM StaffShiftSchedule sc
+INNER JOIN ShiftType t ON sc.ShiftTypeID=t.ShiftTypeID
+INNER JOIN Staff s ON sc.StaffID=s.StaffID
+INNER JOIN StaffPerformanceReview r ON s.StaffID=r.StaffID
+GROUP BY t.ShiftTypeID, t.ShiftName, t.ShiftStartTime, t.ShiftEndTime
+ORDER BY t.Shiftname;
