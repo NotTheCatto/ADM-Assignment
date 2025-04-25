eac603617c811e8cadd4083b5dc4053860ea0cde
Cat on WSL <ykwho098@gmail.com>
2025-04-25 19:04:30 +0800

    feat: add sql for query 2

--- sqlscripts/queries/bei_query2.sql
+++ sqlscripts/queries/bei_query2.sql
@@ -1,19 +1,27 @@
 /*
 Boon Earn Iie - Query 2: List of books available for loaning  WARN: Pending testing
  */
+CLEAR COLUMNS;
+TTITLE ON;
+COLUMN BookID FORMAT 999 JUSTIFY LEFT;
+COLUMN Title FORMAT A20 JUSTIFY LEFT;
+COLUMN Language FORMAT A8 JUSTIFY LEFT;
+COLUMN Qty FORMAT 99 JUSTIFY LEFT;
+COLUMN Available_Copies FORMAT A28 JUSTIFY LEFT;
 
-CLEAR COLUMNS
 -- Set page size and line size
-SET PAGESIZE 1000
-SET LINESIZE 150
-COLUMN "Details of best performing library staffs in last 5 months" FORMAT A20 JUSTIFY CENTER
+SET PAGESIZE 40;
+SET LINESIZE 70;
+SET WRAP ON;
+TTITLE CENTER ' --- List of books available for loaning --- ' FORMAT A20 SKIP 2 LEFT 'Page: ' SQL.PNO SKIP 1;
 
-SELECT b.BookID, b.BookName, COUNT(c.BookCopyID) ,LISTAGG(c.BookCopyID, ',') WITHING GROUP (ORDER BY c.BookCopyID) "Available book copies'' ID", 
-FROM BookCopy c
+SELECT b.BookID, b.Title, l.LanguageName AS Language, COUNT(c.BookCopyID) AS Qty, LISTAGG(c.BookCopyID, ', ') WITHIN GROUP (ORDER BY b.BookID, b.Title, l.LanguageName) "AVAILABLE_COPIES"
+FROM Book b
+INNER JOIN BookCopy c ON b.BookID=c.BookID
 INNER JOIN StatusCode s ON c.StatusCode=s.StatusCode
-INNER JOIN Book b ON c.BookID=b.BookID
-WHERE c.StatusCode = 1
-GROUP BY c.BookCopyID
-ORDER BY c.BookCopyID;
+INNER JOIN LanguageCode l ON b.LanguageCode=l.LanguageCode
+WHERE s.StatusName = 'Available'
+GROUP BY b.BookID, b.Title, l.LanguageName
+ORDER BY l.LanguageName, b.Title;
 
 -- Create view
