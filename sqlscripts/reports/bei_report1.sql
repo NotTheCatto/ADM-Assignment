b251f9e8212843818cebd16bc55c943b77778951
Cat on WSL <ykwho098@gmail.com>
2025-04-26 00:29:39 +0800

    feat: add logic for fetching data
    -	fuck pl sql and its retarded line error indication

--- sqlscripts/reports/bei_report1.sql
+++ sqlscripts/reports/bei_report1.sql
@@ -1,169 +1,97 @@
 /*
 Boon Earn Iie - Report 1: Top 5 performing library staff in last month  WARN: Incomplete
 */
-CREATE OR REPLACE PROCEDURE staff_performance_report IS
-    -- Declare cursor variables
-    TYPE shift_schedule_cursor_type is REF CURSOR;
-    shiftSchedule_cursor shift_schedule_cursor_type;
-
-    -- Declare variables
-    -- Staff table variables
-    v_StaffID Staff.StaffID%TYPE;
-    v_StaffName Staff.StaffName%TYPE;
-    v_HireDate Staff.HireDate%TYPE;
-    v_StaffID Staff.StaffID%TYPE;
-
-    -- Declare cursors
-    CURSOR cursor_staff IS
-	SELECT s.StaffID, s.StaffName, r.RoleName, AVG(p.ReviewScore) AvgScore, a.Street, a.City, a.PostalCode, a.State, a.Country, c.PhoneNo, c.EmailAddress, s.HireDate, s.Gender,
-	    CURSOR (
-		SELECT t.ShiftName, t.ShiftStartTime, t.ShiftEndTime t.ShiftDescription
-		FROM ShiftType t
-		INNER JOIN StaffShiftSchedule sc ON t.ShiftTypeID=sc.ShiftTypeID
-		INNER JOIN Staff s ON sc.StaffID=s.StaffID
-		ORDER BY ShiftID
-	    ) Shift_Info
-	FROM Staff s,
-	INNER JOIN StaffRole r ON s.RoleID=r.RoleID
-	INNER JOIN StaffPerformanceReview p ON s.StaffID=p.StaffID
-	INNER JOIN Address a ON s.AddressID=a.AddressID
-	INNER JOIN Contact a ON s.ContactID=c.ContactID
-	WHERE EXTRACT(month, p.ReviewDate)=EXTRACT(month, SYSDATE)-1
-	GROUP BY StaffID
-	ORDER BY AvgScore
-	LIMIT 5;
-
-BEGIN
-    -- Set variables
-
-    -- Print report title / headings
-    DBMS_OUTPUT.PUT_LINE('Top 5 performing library staff in ' || TO_CHAR(ADD_MONTHS(SYSDATE, -1), 'MONTH') || ' ' || EXTRACT(YEAR FROM ADD_MONTHS(SYSDATE, -1)));
-    --DBMS_OUTPUT.PUT_LINE(CHR(10));
-    DBMS_OUTPUT.PUT_LINE('Date Printed: ' || TO_CHAR(SYSDATE, 'dd-Mon-yyyy'));
-    --DBMS_OUTPUT.PUT_LINE(CHR(10));
-
-    -- Open cursor
-    OPEN cursor_staff
-
-    -- Loop
-    LOOP
-
-	-- Fetch
-	FETCH cursor_staff INTO v_CustNo, v_CustName, v_ContactPhone, v_SalesRepEmpNo, v_SalesRepEmpName, cursor_order;
-
-	-- Break loop if cursor fetches nothing
-	EXIT WHEN <cursor>$NOTFOUND;
-
-	-- Print
-
-    -- End loop
-    END LOOP
-
-    -- Output grand total / summary
-
-    -- Close cursor
-END;
-/
-
+SET PAGESIZE 0;
+SET SERVEROUTPUT ON;
 
 CREATE OR REPLACE PROCEDURE staff_performance_report IS
+	-- Declare cursor variables
+	TYPE shift_schedule_cursor_type is REF CURSOR;
+	shiftSchedule_cursor shift_schedule_cursor_type;
 
-     TYPE order_cursor_typ IS REF CURSOR;
-     cursor_order    order_cursor_typ;
+	-- Declare variables
+	-- Staff table variables
 
-     --From Customers/Employees Table
-     v_CustNo Customers.CustomerNumber%TYPE;
-     v_CustName Customers.CustomerName%TYPE;
-     v_ContactPhone Customers.Phone%TYPE;
-     v_SalesRepEmpNo Customers.SalesRepEmployeeNumber%TYPE;
-     v_SalesRepEmpName VARCHAR(60);
+	v_StaffID Staff.StaffID%TYPE;
+	v_StaffFirstName Staff.StaffFirstName%TYPE;
+	v_StaffLastName Staff.StaffLastName%TYPE;
+	v_RoleName StaffRole.RoleName%TYPE;
+	v_AvgScore StaffPerformanceReview.ReviewScore%TYPE;
+	v_Street Address.Street%TYPE;
+	v_City Address.City%TYPE;
+	v_PostalCode Address.PostalCode%TYPE;
+	v_State Address.State%TYPE;
+	v_Country Address.Country%TYPE;
+	v_PhoneNo Contact.PhoneNo%TYPE;
+	v_EmailAddress Contact.EmailAddress%TYPE;
+	v_HireDate Staff.HireDate%TYPE;
+	v_StaffGender Staff.StaffGender%TYPE;
 
-     -- From Orders table
-     v_OrderNo Orders.OrderNumber%TYPE;
-     v_OrderDate Orders.OrderDate%TYPE;
-     v_FinalTotal Orders.FinalTotal%TYPE;
+	v_Shiftname ShiftType.ShiftName%TYPE;
+	v_ShiftStartTime ShiftType.ShiftStartTime%TYPE;
+	v_ShiftEndTime ShiftType.ShiftEndTime%TYPE;
+	v_ShiftDescription ShiftType.ShiftDescription%TYPE;
 
-     --For Report
-     v_GST NUMBER;
-     v_TotalCustAmt NUMBER;
-     v_TotalCustGST NUMBER;
-     v_NoOfCust NUMBER;
-     v_GrandTotalSpending NUMBER;
-     v_GrandTotalGST NUMBER;
-     v_CountOrder   NUMBER;
-     v_CountCustomer NUMBER;
-
-
-
-     CURSOR cursor_cust IS
-     SELECT DISTINCT c.CustomerNumber, CustomerName,  Phone, SalesRepEmployeeNumber, e.FirstName || ' ' || e.LastName As "SalesRepEmpName",
-		CURSOR(
-			SELECT o.OrderNumber, o.OrderDate, o.FinalTotal
-			FROM Orders o
-			WHERE c.CustomerNumber = o.CustomerNumber
-			AND EXTRACT(YEAR FROM o.OrderDate) = EXTRACT(YEAR FROM SYSDATE)
-			AND EXTRACT (MONTH FROM o.OrderDate) = EXTRACT(MONTH FROM SYSDATE)
-			ORDER BY o.OrderNumber
-		) Order_Info
-     FROM Customers c, Employees e, Orders o1
-     WHERE c.SalesRepEmployeeNumber = e.EmployeeNumber
-     AND c.CustomerNumber = o1.CustomerNumber
-    AND EXTRACT(YEAR FROM o1.OrderDate) = EXTRACT(YEAR FROM SYSDATE)
-    AND EXTRACT (MONTH FROM o1.OrderDate) = EXTRACT(MONTH FROM SYSDATE);
-
-
+	-- Declare cursors
+	CURSOR cursor_staff IS
+		SELECT * FROM (
+			SELECT s.StaffID, s.StaffFirstName, s.StaffLastName, r.RoleName, AVG(p.ReviewScore) AvgScore, a.Street, a.City, a.PostalCode, a.State, a.Country, c.PhoneNo, c.EmailAddress, s.HireDate, s.StaffGender
+-- 			CURSOR (
+-- 				SELECT t.ShiftName, TO_CHAR(t.ShiftStartTime,'hh24:mi:ss'), TO_CHAR(t.ShiftEndTime,'hh24:mi:ss'), t.ShiftDescription
+-- 				FROM ShiftType t
+-- 				INNER JOIN StaffShiftSchedule sc ON t.ShiftTypeID=sc.ShiftTypeID
+-- 				INNER JOIN Staff s ON sc.StaffID=s.StaffID
+-- 				ORDER BY ShiftID
+-- 			) Shift_Info
+			FROM Staff s
+			INNER JOIN StaffRole r ON s.RoleID=r.RoleID
+			INNER JOIN StaffPerformanceReview p ON s.StaffID=p.StaffID
+			INNER JOIN Address a ON s.AddressID=a.AddressID
+			INNER JOIN Contact c ON s.ContactID=c.ContactID
+			WHERE EXTRACT(MONTH FROM p.ReviewDate)=EXTRACT(MONTH FROM SYSDATE)-1
+			GROUP BY s.StaffID, s.StaffFirstName, s.StaffLastName, r.RoleName, a.Street, a.City, a.PostalCode, a.State, a.Country, c.PhoneNo, c.EmailAddress, s.HireDate, s.StaffGender
+			ORDER BY AvgScore DESC
+		) WHERE ROWNUM <= 5;
 
 BEGIN
-	v_GST := 0;
-	v_TotalCustAmt := 0;
-	v_TotalCustGST := 0;
-	v_NoOfCust := 0;
-	v_GrandTotalSpending := 0;
-	v_GrandTotalGST := 0;
-	v_CountOrder   := 0;
-	v_CountCustomer :=0;
+	-- Set variables
 
-	DBMS_OUTPUT.PUT_LINE('CUSTOMERS MONTHLY ORDER REPORT FOR THE MONTH OF ' || TO_CHAR(SYSDATE, 'MONTH') || ' ' || EXTRACT(YEAR FROM SYSDATE));
+	-- Print report title / headings
+	DBMS_OUTPUT.PUT_LINE('Top 5 performing library staff in ' || TO_CHAR(ADD_MONTHS(SYSDATE, -1), 'MONTH') || ' ' || EXTRACT(YEAR FROM ADD_MONTHS(SYSDATE, -1)));
 	--DBMS_OUTPUT.PUT_LINE(CHR(10));
 	DBMS_OUTPUT.PUT_LINE('Date Printed: ' || TO_CHAR(SYSDATE, 'dd-Mon-yyyy'));
 	--DBMS_OUTPUT.PUT_LINE(CHR(10));
 
-	OPEN cursor_cust;
+	-- Open cursor
+	OPEN cursor_staff;
+
+	-- Loop
 	LOOP
-	     FETCH cursor_cust INTO v_CustNo, v_CustName, v_ContactPhone, v_SalesRepEmpNo, v_SalesRepEmpName, cursor_order;
-	     EXIT WHEN cursor_cust%NOTFOUND;
-		v_NoOfCust := v_NoOfCust + 1;
-		DBMS_OUTPUT.PUT_LINE(CHR(10));
-		DBMS_OUTPUT.PUT_LINE('Customer No : ' || v_CustNo);
-		DBMS_OUTPUT.PUT_LINE('Customer Name : ' || v_CustName);
-		DBMS_OUTPUT.PUT_LINE('Contact No : ' || v_ContactPhone);
-		DBMS_OUTPUT.PUT_LINE('Sales Rep No/Name: ' || v_SalesRepEmpNo || ' - ' || v_SalesRepEmpName);
-		DBMS_OUTPUT.PUT_LINE(RPAD('.',80,'.'));
-
-		v_CountOrder := 0; --reset no. of orders for each customer
-		--=====================================
-		-- Display Orders for each customer
-		--=====================================
-		v_TotalCustAmt := 0.0;
-		v_TotalCustGST := 0.0;
-
+		-- Fetch
+		FETCH cursor_staff INTO v_StaffID, v_StaffFirstName, v_StaffLastName, v_RoleName, v_AvgScore, v_Street, v_City, v_PostalCode, v_State, v_Country, v_PhoneNo, v_EmailAddress, v_HireDate, v_StaffGender;
+	
+		-- Break loop if cursor fetches nothing
+		EXIT WHEN cursor_staff%NOTFOUND;
+	
+		-- Print
+		OPEN shiftSchedule_cursor FOR 'SELECT t.ShiftName, t.ShiftStartTime, t.ShiftEndTime, t.ShiftDescription
+		FROM ShiftType t
+		INNER JOIN StaffShiftSchedule sc ON t.ShiftTypeID=sc.ShiftTypeID
+		INNER JOIN Staff s ON sc.StaffID=' || v_StaffID || '
+		ORDER BY ShiftID';
 		LOOP
-		FETCH cursor_order INTO v_OrderNo, v_OrderDate, v_FinalTotal;
-		EXIT WHEN cursor_order%NOTFOUND;
-			v_CountOrder := v_CountOrder + 1;
-			v_TotalCustAmt := v_TotalCustAmt + v_FinalTotal;
-			v_GST := v_FinalTotal * 0.06;
-			v_TotalCustGST := v_TotalCustGST + v_GST;
-			DBMS_OUTPUT.PUT_LINE(RPAD((v_CountOrder || '.  Order Date : ' || v_OrderDate),30));
-			DBMS_OUTPUT.PUT_LINE(RPAD('Final Total: RM ' || TO_CHAR(v_FinalTotal,'$99,999.99'),30));
-			DBMS_OUTPUT.PUT_LINE(RPAD('GST Amount: RM ' || TO_CHAR(v_GST,'$99,999.99'),30));
-			DBMS_OUTPUT.PUT_LINE(CHR(10));
+			FETCH shiftSchedule_cursor INTO v_Shiftname, v_ShiftStartTime, v_ShiftEndTime, v_ShiftDescription;
+			EXIT WHEN shiftSchedule_cursor%NOTFOUND;
+
 		END LOOP;
-		v_GrandTotalSpending := v_GrandTotalSpending + v_TotalCustAmt;
-		v_GrandTotalGST := v_GrandTotalGST + v_TotalCustGST;
+		CLOSE shiftSchedule_cursor;
+	
+	-- End loop
 	END LOOP;
-	CLOSE cursor_cust;
+
+	-- Output grand total / summary
+
+	-- Close cursor
 END;
 /
-
 
