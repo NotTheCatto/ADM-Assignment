9aee8ddc49ba948ca2a74e75250882d3ce60adcb
Cat on WSL <ykwho098@gmail.com>
2025-04-25 16:25:14 +0800

    feat: adjust initial line size

--- sqlscripts/queries/bei_query1.sql
+++ sqlscripts/queries/bei_query1.sql
@@ -1,19 +1,18 @@
 /*
 Boon Earn Iie - Query 1: Summary of staff shift schedule and details of assigned staff
 */
-CLEAR COLUMNS
-CLEAR TTITLE
-TTITLE ON
-COLUMN ShiftTypeID FORMAT 999 JUSTIFY LEFT
-COLUMN ShiftName FORMAT A9 JUSTIFY LEFT
-COLUMN StartTime FORMAT A9 JUSTIFY LEFT
-COLUMN EndTime FORMAT A9 JUSTIFY LEFT
-COLUMN NumOfStaff FORMAT 999999999 JUSTIFY LEFT
-COLUMN StaffAvgScore FORMAT 999.99 JUSTIFY LEFT
+CLEAR COLUMNS;
+TTITLE ON;
+COLUMN ShiftTypeID FORMAT 999 JUSTIFY LEFT;
+COLUMN ShiftName FORMAT A9 JUSTIFY LEFT;
+COLUMN StartTime FORMAT A9 JUSTIFY LEFT;
+COLUMN EndTime FORMAT A9 JUSTIFY LEFT;
+COLUMN NumOfStaff FORMAT 999999999 JUSTIFY LEFT;
+COLUMN StaffAvgScore FORMAT 999.99 JUSTIFY LEFT;
 
--- Set page size and line size
-SET PAGESIZE 1000
-SET LINESIZE 500
+-- Set page size and line size;
+SET PAGESIZE 300;
+SET LINESIZE 66;
 TTITLE CENTER ' --- Summary of staff shift schedule and assigned staff --- ' FORMAT A20 SKIP 2 LEFT 'Page: ' SQL.PNO SKIP 1;
 
 SELECT t.ShiftTypeID, t.ShiftName, TO_CHAR(t.ShiftStartTime,'hh24:mi:ss') AS StartTime, TO_CHAR(t.ShiftEndTime,'hh24:mi:ss') AS EndTime, COUNT(sc.StaffID) AS NumOfStaff, ROUND(AVG(r.ReviewScore), 2) AS StaffAvgScore
